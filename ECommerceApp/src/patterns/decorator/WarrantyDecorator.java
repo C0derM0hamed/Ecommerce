0f8a1d419723974935deee1d09e588ac906c0b2b
C0derM0hamed <mohamedmostafa2011360@gmail.com>
2024-12-19 21:26:24 +0200

    make comments

--- ECommerceApp/src/patterns/decorator/WarrantyDecorator.java
+++ ECommerceApp/src/patterns/decorator/WarrantyDecorator.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package patterns.decorator;
 
 import models.ProductComponent;
@@ -12,7 +7,7 @@ import models.ProductComponent;
  * @author moham
  */
 public class WarrantyDecorator extends ProductDecorator {
-    private static final double WARRANTY_COST = 15.0;
+    private static final double WARRANTY_COST = 20.0;
 
     public WarrantyDecorator(ProductComponent product) {
         super(product);
